79e610dac4fb6c490f9c782931d49271be04e4c1
Alejo Ceballos <alejoceballos75@gmail.com>
2025-01-14 17:08:54 +0000

    Section 8: Service Discovery & Service Registration in microservices (Lectures 103 to 109)
    - Got Eureka Server to work
    - Check the new eurekaserver.yml in micorservices-study-config repo

---
+++ eurekaserver/src/test/java/com/momo2x/momobank/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.momo2x.momobank.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
